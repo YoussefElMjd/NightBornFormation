d5534fda125034bd14912338f9cd13fcba077f49
Youssef El Majdoul <56172@etu.he2b.be>
2023-02-07 16:19:27 +0100

    part 1 - d - ex 1.7-1.10

--- train-app/src/App.js
+++ train-app/src/App.js
@@ -4,26 +4,73 @@ const Button = (props) => {
   return <button onClick={props.increment}> {props.react.name}</button>;
 };
 
-const Stats = (props) => {
+const React = (props) => {
   return <p>{props.name + " " + props.click}</p>;
 };
+
+const Stats = (props) => {
+  const allReact = props.good.click + props.neutdal.click + props.bad.click;
+  if (allReact == 0) {
+    return <p>No feedback given</p>;
+  }
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <React name={props.good.name} click={props.good.click} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <React name={props.neutdal.name} click={props.neutdal.click} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <React name={props.bad.name} click={props.bad.click} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>average {(props.good.click - props.bad.click) / allReact}</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>positive {(props.good.click / allReact) * 100} %</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 const App = () => {
-  // enregistrer les clics de chaque bouton dans un état différent
+  // enregistder les clics de chaque bouton dans un état différent
   const [good, setGood] = useState({ name: "good", click: 0 });
-  const [neutral, setNeutral] = useState({ name: "neutral", click: 0 });
+  const [neutdal, setNeutdal] = useState({ name: "neutdal", click: 0 });
   const [bad, setBad] = useState({ name: "bad", click: 0 });
 
   return (
     <div>
       <h1> give feedback</h1>
-      <Button react={good} increment={() => setGood({...good,click : good.click+1})} />
-      <Button react={neutral} increment={() => setNeutral({...neutral,click : neutral.click+1})} />
-      <Button react={bad} increment={() => setBad({...bad,click : bad.click+1})} />
+      <Button
+        react={good}
+        increment={() => setGood({ ...good, click: good.click + 1 })}
+      />
+      <Button
+        react={neutdal}
+        increment={() => setNeutdal({ ...neutdal, click: neutdal.click + 1 })}
+      />
+      <Button
+        react={bad}
+        increment={() => setBad({ ...bad, click: bad.click + 1 })}
+      />
 
       <h1>statistics</h1>
-      <Stats name={good.name} click={good.click} />
-      <Stats name={neutral.name} click={neutral.click} />
-      <Stats name={bad.name} click={bad.click} />
+      <Stats good={good} neutdal={neutdal} bad={bad} />
     </div>
   );
 };
