34f65c44585ef7c6353f5b42a7bd2790ca4aab20
Youssef El Majdoul <56172@etu.he2b.be>
2023-02-07 16:25:31 +0100

    part 1 - d - ex 1.12*

--- train-app/src/App.js
+++ train-app/src/App.js
@@ -1,76 +1,25 @@
 import { useState } from "react";
 
-const Button = (props) => {
-  return <button onClick={props.increment}> {props.react.name}</button>;
-};
-
-const React = (props) => {
-  return <p>{props.name + " " + props.click}</p>;
-};
-
-const Stats = (props) => {
-  const allReact = props.good.click + props.neutdal.click + props.bad.click;
-  if (allReact == 0) {
-    return <p>No feedback given</p>;
-  }
-  return (
-    <div>
-      <table>
-        <tbody>
-          <tr>
-            <td>
-              <React name={props.good.name} click={props.good.click} />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <React name={props.neutdal.name} click={props.neutdal.click} />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <React name={props.bad.name} click={props.bad.click} />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <p>average {(props.good.click - props.bad.click) / allReact}</p>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <p>positive {(props.good.click / allReact) * 100} %</p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-};
 const App = () => {
-  // enregistder les clics de chaque bouton dans un état différent
-  const [good, setGood] = useState({ name: "good", click: 0 });
-  const [neutdal, setNeutdal] = useState({ name: "neutdal", click: 0 });
-  const [bad, setBad] = useState({ name: "bad", click: 0 });
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+  ];
 
+  const [selected, setSelected] = useState(0);
+
+  const randomNumber = () => {
+    setSelected(Math.floor(Math.random() * ((anecdotes.length-1) - 0 + 1) + 0));
+  };
   return (
     <div>
-      <h1> give feedback</h1>
-      <Button
-        react={good}
-        increment={() => setGood({ ...good, click: good.click + 1 })}
-      />
-      <Button
-        react={neutdal}
-        increment={() => setNeutdal({ ...neutdal, click: neutdal.click + 1 })}
-      />
-      <Button
-        react={bad}
-        increment={() => setBad({ ...bad, click: bad.click + 1 })}
-      />
-
-      <h1>statistics</h1>
-      <Stats good={good} neutdal={neutdal} bad={bad} />
+      <h1>{anecdotes[selected]}</h1>
+      <button onClick={randomNumber}>next anecdote</button>
     </div>
   );
 };
